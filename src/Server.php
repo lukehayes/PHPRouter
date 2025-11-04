2d6accdda2d0d739fb881249904d7dd692c4965c
lukehayes <lukepurplehayes@gmail.com>
2025-11-04 09:59:43 +0000

    feat: add LDH\Server

---
+++ src/Server.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LDH;
+
+/**
+ * Class is purely for testing using PHPunit.
+ *
+ * Need to change to Symfony/HttpFoundation.
+*/
+class Server
+{
+	static public $REQUEST_URI = '/';
+	static public $REQUEST_METHOD = 'GET';
+}
